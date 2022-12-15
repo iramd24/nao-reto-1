7f262c76afd48d51d0d29295df4cea2e7c919cf0
iramd24 <idr.diaz96@gmail.com>
2022-12-15 01:54:25 -0600

    added document.ready function to avoid onSubmit func is not defined error

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -5,20 +5,23 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+let sendForm;
 
-let sendForm = () => {
-    let emailSub = 'Solicitud de registro a curso ' + document.getElementById('nameInput').value;
-    let emailBody = 'Nombre: ' + document.getElementById('nameInput').value + '\r\n' + 
-                    'Número de teléfono: ' + document.getElementById('cellInput').value + '\r\n' +
-                    'Correo: ' + document.getElementById('mailInput').value + '\r\n';
-
-    emailBody = encodeURIComponent(emailBody);
-    location.href = 'mailto:test@mail.com?subject='+emailSub+'&body='+emailBody;
-
-
-    const inputs = document.querySelectorAll('#nameInput, #cellInput, #mailInput');
-
-    inputs.forEach(input => {
-        input.value = '';
-    });
-}
+$(document).ready(function() { 
+    sendForm = () => {
+        let emailSub = 'Solicitud de registro a curso ' + document.getElementById('nameInput').value;
+        let emailBody = 'Nombre: ' + document.getElementById('nameInput').value + '\r\n' + 
+                        'Número de teléfono: ' + document.getElementById('cellInput').value + '\r\n' +
+                        'Correo: ' + document.getElementById('mailInput').value + '\r\n';
+    
+        emailBody = encodeURIComponent(emailBody);
+        location.href = 'mailto:test@mail.com?subject='+emailSub+'&body='+emailBody;
+    
+    
+        const inputs = document.querySelectorAll('#nameInput, #cellInput, #mailInput');
+    
+        inputs.forEach(input => {
+            input.value = '';
+        });
+    }
+});
